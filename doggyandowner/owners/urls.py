a883b1c46dcec17b563ab6e47df5b1162deaff61
lesol <wingnawing@gmail.com>
2022-02-14 20:45:03 +0900

    ADD : owners app GET 완료

--- doggyandowner/owners/urls.py
+++ doggyandowner/owners/urls.py
@@ -4,5 +4,7 @@ from owners.views import *
 
 urlpatterns = [
     path('owner_register', ownersView.as_view()),
-    path('dog_register', dogsView.as_view())
+    path('dog_register', dogsView.as_view()),
+    path('owners_and_dogs', owners_dogs_View.as_view())
 ]
+ 
--- doggyandowner/owners/views.py
+++ doggyandowner/owners/views.py
@@ -18,6 +18,24 @@ class ownersView(View):
 
         return JsonResponse({'messasge':'owner created!'}, status=201)
 
+
+    def get(self, request):
+        owners_list = Owner.objects.all()
+        results  = []
+
+        for i in owners_list:
+           results.append(
+               {
+                    "name" : i.name,
+                    "email" : i.email,
+                    "age" : i.age
+               }
+           )
+       
+        return JsonResponse({'results':results}, status=200)
+
+
+
 class dogsView(View):
     def post(self, request):
         data = json.loads(request.body)
@@ -30,3 +48,47 @@ class dogsView(View):
         )
 
         return JsonResponse({'messasge':'dog created!'}, status=201)
+
+
+    def get(self, request):
+        dogs_list = Dog.objects.all()
+        results  = []
+
+        for i in dogs_list:
+           results.append(
+               {
+                    "name" : i.name,
+                    "age" : i.age,
+                    "owner" : Owner.objects.get(id=i.owner_id).name
+               }
+           )
+       
+        return JsonResponse({'results':results}, status=200)
+
+
+class owners_dogs_View(View):
+    def get(self, request):
+        results  = []
+
+        owners_list = Owner.objects.all()
+
+        for i in owners_list:
+
+            dog_of_owner = Dog.objects.filter(owner_id=i.id)
+            dogs = {}
+            dog_list = []
+            for j in dog_of_owner:
+                dogs = {"name":j.name, "age":j.age}
+                dog_list.append(dogs)
+                    
+            results.append(
+                {
+                    "name" : i.name,
+                    "email" : i.email,
+                    "age" : i.age,
+                    "dogs" : dog_list
+                }
+            )
+
+        return JsonResponse({'results':results}, status=200)
+
