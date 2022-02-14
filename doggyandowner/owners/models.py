a1286e00e2cf33fffb4d217ff3e4cca4110cf8d5
lesol <wingnawing@gmail.com>
2022-02-14 18:41:28 +0900

    ADD : owners app POST 완료

--- doggyandowner/owners/models.py
+++ doggyandowner/owners/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Dog(models.Model):
+    name = models.CharField(max_length=45)
+    age = models.IntegerField()
+    owner = models.ForeignKey("Owner", on_delete=models.CASCADE) 
+
+    class Meta:
+        db_table = 'dogs'
+
+class Owner(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=300)
+    age = models.IntegerField()
+
+    class Meta:
+        db_table = 'owners'
